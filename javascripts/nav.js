9566ebccf598d0f5570c2aa091a25e9ba9d705cc
GreekLaw <GreekLaw@users.noreply.github.com>
2014-08-07 20:21:09 +0300

    Update nav.js

--- javascripts/nav.js
+++ javascripts/nav.js
@@ -1,74 +1,8 @@
-void function(){
-  var nav = document.getElementsByTagName('nav')[0]
-  nav.innerHTML = 
-                                                                                                                   '\
-  <ul class="favorites">                                                                                            \
-    <li><a href="./επιταγή.html">Επιταγή</a></li>                                                                    \
-    <li><a href="./συναλλαγματική.html">Συναλλαγματική</a></li>                                                     \
-    <li><a href="./επίδοση.html">Επίδοση</a></li>                                                                    \
-    <li><a href="./επικύρωση-μετάφραση.html">Επικύρωση</a></li>                                                    \
-    <li><a href="./επικύρωση-μετάφραση.html#μετάφραση">Μετάφραση</a></li>                                         \
-    <li><a href="./προθεσμίες.html">Προθεσμίες</a></li>                                                               \
-    <li><a href="./τέλος-απογράφου-δικαστικό-ένσημο-τόκοι.html">Τέλος απογράφου</a></li>                            \
-    <li><a href="./τέλος-απογράφου-δικαστικό-ένσημο-τόκοι.html#δικαστικό-ένσημο">Δικαστικό ένσημο</a></li>           \
-    <li><a href="./τέλος-απογράφου-δικαστικό-ένσημο-τόκοι.html#τόκοι">Τόκοι</a></li>                                 \
-  </ul>                                                                                                            \
-  <ul><header>ΑΣΤΙΚΑ</header>                                                                                      \
-    <li><a href="./αδικαιολόγητος-πλουτισμός.html">Αδικαιολόγητος πλουτισμός</a></li>                                \
-    <li><a href="./αδικοπραξία.html">Αδικοπραξία</a></li>                                                           \
-    <li><a href="./αξιόγραφα.html">Αξιόγραφα</a>                                                                  \
-      <ul><li><a href="./επιταγή.html">Επιταγή</a></li>                                                             \
-        <li><a href="./συναλλαγματική.html">Συναλλαγματική</a></li>                                                \
-        <li><a href="./χρεωστικό-ομόλογο.html">Χρεωστικό ομόλογο</a></li>                                          \
-      </ul></li>                                                                                                    \
-    <li><a href="./αποδεικτικά-μέσα.html">Αποδεικτικά μέσα</a></li>                                                  \
-    <li><a href="./ασφαλιστικά-μέτρα.html">Ασφαλιστικά μέτρα</a></li>                                              \
-    <li><a href="./δικαστική-συμπαράσταση.html">Δικαστική συμπαράσταση</a></li>                                    \
-    <li><a href="./έφεση-αστικής-απόφασης.html">Έφεση αστικής απόφασης</a></li>                                    \
-    <li><a href="./αναίρεση.html">Αναίρεση</a></li>                                                                 \
-    <li><a href="./επίδοση.html">Επίδοση</a></li>                                                                   \
-    <li><a href="./καταδολίευση-δανειστών.html">Καταδολίευση δανειστών</a></li>                                     \
-    <li><a href="./μικροδιαφορές.html">Μικροδιαφορές</a></li>                                                        \
-    <li><a href="./μίσθωση.html">Μίσθωση</a>                                                                        \
-      <ul><li><a href="./αγρομίσθωση.html">Αγρομίσθωση</a></li>                                                    \
-        <li><a href="./εμπορική-μίσθωση.html">Εμπορική μίσθωση</a></li>                                             \
-        <li><a href="./μίσθωση-πράγματος.html">Μίσθωση πράγματος</a></li>                                          \
-      </ul></li>                                                                                                     \
-    <li><a href="./περάτωση-της-δίκης.html">Περάτωση της δίκης</a></li>                                             \
-    <li><a href="./πώληση.html">Πώληση</a>                                                                        \
-      <ul><li><a href="./πώληση-πράγματος.html">Πώληση πράγματος</a></li>                                        \
-        <li><a href="./πώληση-προς-καταναλωτή.html">Πώληση προς καταναλωτή</a></li>                             \
-        <li><a href="./εξώνηση-ανταλλαγή-πώληση-με-δοκιμή.html">Εξώνηση, ανταλλαγή, πώληση με δοκιμή</a></li>    \
-      </ul></li>                                                                                                    \
-    <li><a href="./υπερχρεωμένα-νοικοκυριά.html">Υπερχρεωμένα νοικοκυριά</a>                                      \
-    </ul>                                                                                                           \
-    <ul><header>ΠΟΙΝΙΚΑ</header>                                                                                    \
-      <li><a href="./ποινική-δίωξη.html">Ποινική δίωξη</a>                                                              \
-        <ul><li><a href="./ισχυρισμοί-της-υπεράσπισης.html">Ισχυρισμοί της υπεράσπισης</a></li>                         \
-          <li><a href="./πλαστογραφία.html">Πλαστογραφία</a></li>                                                    \
-        </ul></li>                                                                                                     \
-    </ul>                                                                                                              \
-    <ul><header>ΕΞΩΔΙΚΑΣΤΙΚΑ</header>                                                                                  \
-      <li><a href="./δικηγορικές-αμοιβές.html">Δικηγορικές αμοιβές</a></li>                                                           \
-      <li><a href="./αντιπροσώπευση-πληρεξουσιότητα.html">Αντιπροσώπευση - πληρεξουσιότητα</a></li>                    \
-      <li><a href="./εκτέλεση.html">Εκτέλεση</a></li>                                                                   \
-      <li><a href="./επικύρωση-μετάφραση.html">Επικύρωση - Μετάφραση</a></li>                                        \
-      <li><a href="./προθεσμίες.html">Προθεσμίες</a></li>                                                               \
-      <li><a href="./συμβόλαια-συμφωνητικά.html">Συμβόλαια - Συμφωνητικά</a></li>                                    \
-      <li><a href="./τέλος-απογράφου-δικαστικό-ένσημο-τόκοι.html">Τέλος απογράφου - Δικαστικό ένσημο - Τόκοι</a></li>  \
-      <li><a href="./υποθηκοφυλάκειο.html">Υποθηκοφυλάκειο</a></li>                                                   \
-      <li><a href="./λεξικό.html">Λεξικό</a></li>                                                                       \
-   </ul>                                                                                                               \
-  '
-  
-  
-  void function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  }(window,document,'script','//www.google-analytics.com/analytics.js','ga')
+void function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+}(window,document,'script','//www.google-analytics.com/analytics.js','ga')
 
-  ga('create', 'UA-33952776-3', 'greeklaw.github.io')
-  ga('require', 'displayfeatures')
-  ga('send', 'pageview')
-
-}()
+ga('create', 'UA-33952776-3', 'greeklaw.github.io')
+ga('require', 'displayfeatures')
+ga('send', 'pageview')
