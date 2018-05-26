7d70b55ada76e1a4aeb664245c9b4b892bb2a3e3
GreekLaw <GreekLaw@users.noreply.github.com>
2018-05-26 17:51:12 +0300

    Update nav.js

--- javascripts/nav.js
+++ javascripts/nav.js
@@ -4,7 +4,7 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 }(window,document,'script','//www.google-analytics.com/analytics.js','ga')
 
 ga('create', 'UA-33952776-3', 'greeklaw.github.io')
-ga('require', 'displayfeatures')
+ga('set', 'anonymizeIp', true)
 ga('send', 'pageview')
 
 document.addEventListener('click', handleIt)
